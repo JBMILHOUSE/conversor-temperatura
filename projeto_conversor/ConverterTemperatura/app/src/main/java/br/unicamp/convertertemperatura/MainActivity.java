a6a6ad3d65618a4840fc63c36217c9127f9341bc
JBMILHOUSE <43553799+JBMILHOUSE@users.noreply.github.com>
2020-06-23 13:47:18 -0300

    incompleto
    ainda em desenvolvimento

---
+++ projeto_conversor/ConverterTemperatura/app/src/main/java/br/unicamp/convertertemperatura/MainActivity.java
@@ -0,0 +1,49 @@
+package br.unicamp.convertertemperatura;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tvMensagens;
+    EditText edtCelsius;
+    Button btnConverter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        edtCelsius = (EditText)findViewById(R.id.edtCelsius);
+        tvMensagens = findViewById(R.id.tvMensagens);
+        btnConverter = findViewById(R.id.btnConverter);
+
+        btnConverter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(edtCelsius.getText().toString().equals("")){
+
+                }else{
+                    tvMensagens.setText("");
+                    double varCelsius = Double.parseDouble(edtCelsius.getText().toString());
+
+                    Intent intent = new Intent(MainActivity.this, resultado.class);
+                    Bundle params = new Bundle();
+                    Double msgCelsius = Double.parseDouble(edtCelsius.getText().toString());
+
+                    params.putDouble("varCelsius", msgCelsius);
+
+                    intent.putExtras(params);
+                    startActivity(intent);
+
+                }
+            }
+        });
+    }
+}
---
+++ projeto_conversor/ConverterTemperatura/app/src/main/java/br/unicamp/convertertemperatura/resultado.java
@@ -0,0 +1,34 @@
+package br.unicamp.convertertemperatura;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class resultado extends AppCompatActivity {
+
+    TextView tvResultadoFahrenheit;
+    TextView tvResultadoKelvin;
+
+   // double calKel = 273.15;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resultado);
+
+        Intent intent = getIntent();
+        Bundle params = intent.getExtras();
+        Double varFahrenheit = params.getDouble("varCelsius");
+        Double varKelvin = params.getDouble("varCelsius");
+
+        tvResultadoFahrenheit = findViewById(R.id.tvResultadoFahrenheit);
+        tvResultadoKelvin = findViewById(R.id.tvResultadoKelvin);
+
+        tvResultadoFahrenheit.setText(String.valueOf((varFahrenheit * 1.8) + 32));
+        tvResultadoKelvin.setText((String.valueOf(varKelvin + 273.15)));
+
+        // varFahrenheit = (celsius* 1.8) + 32
+        // varKelvin = celsius + 273.15
+    }
+}
